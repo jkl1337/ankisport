9bc0c4a420238500420d070535077a3c4f1688be
John K. Luebs <john.luebs@accunurse.com>
2017-04-08 23:59:47 -0400

    Implement settings load and standalone dialog

--- __init__.py
+++ __init__.py
@@ -4,16 +4,17 @@ import re
 import textwrap
 from collections import defaultdict
 from datetime import datetime
-from itertools import islice
+from itertools import islice, izip
 
 from PyQt4 import QtCore, QtGui
 from anki.exporting import Exporter
 from anki.hooks import addHook
 from anki.lang import _
-from anki.utils import splitFields
+from anki.utils import splitFields, ids2str
 from aqt import mw
 from aqt.qt import *
-
+from aqt.utils import showWarning
+import pytoml as toml
 
 class TOMLGenerator(object):
     g_newline = '\n'
@@ -161,119 +162,181 @@ class TOMLNoteExporter(Exporter):
     key = _("Notes in TOML format")
     ext = ".toml"
 
-    def __init__(self, col):
+    def __init__(self, col, query=None, sets=None):
         Exporter.__init__(self, col)
         self.includeMedia = True
+        self.query = query
+        self.sets = sets
 
     def exportInto(self, path):
         file = codecs.open(path, "w", encoding='utf-8')
         self.doExport(file)
         file.close()
 
-    def doExport(self, output):
-        generator = TOMLGenerator(output)
+    def doExport(self, path):
         models = self.col.models
         outputModels = keydefaultdict(lambda mid: OutputModel(models, mid))
 
-        cardIds = self.cardIds()
         count = 0
 
-        #        """SELECT guid, flds, mid, tags
-        # FROM notes
-        # WHERE id IN
-        #  (SELECT nid
-        #   FROM cards
-        #   WHERE cards.id IN %s)
-        # ORDER BY sfld""" % ids2str(cardIds)
-        # r"""
-        # SELECT guid, flds, mid, tags FROM notes
-        # WHERE tags LIKE '%Brosencephalon%' ORDER BY sfld"""
-        # """
+        notes = []
+        query = self.query
+        if query is not None:
+            sets = self.sets if self.sets else ['']
+            for s in sets:
+                notes.append(self.col.findNotes(query + ' (tag:%s or tag:%s::*)' % (s, s)))
+        else:
+            cardIds = self.cardIds()
+            cursor = self.col.db.execute(r"""
+SELECT guid, flds, mid, tags
+FROM notes
+WHERE id IN
+  (SELECT nid
+   FROM cards
+   WHERE cards.id IN %s)
+ORDER BY sfld""" % ids2str(cardIds))
 
-        for id, flds, mid, tags in self.col.db.execute(r"""
+        for note_ids, group_name in izip(notes, sets):
+            cur_path, ext = os.path.splitext(path)
+            cur_path = '%s-%s%s' % (cur_path, group_name, ext)
+            with codecs.open(cur_path, 'w', encoding='utf-8') as output:
+                generator = TOMLGenerator(output)
+
+                for id, flds, mid, tags in self.col.db.execute(r"""
 SELECT guid, flds, mid, tags FROM notes
-         WHERE tags LIKE '%Brosencephalon%' ORDER BY sfld"""):
-            fieldData = splitFields(flds)
-            om = outputModels[mid]
-            output.write('[[note]]\n')
-            output.write("model = '%s'\n" % om.name)
-            for i, name in enumerate(om.fieldNames):
-                f = fieldData[i]
-                if name == u'note-id':
-                    f = int(f)
-                generator.gen_key_value(name, f)
-            generator.gen_key_value(u'tags', tags.strip().decode('utf-8'))
-            output.write('\n')
-            count += 1
+WHERE id IN %s
+ORDER BY sfld""" % ids2str(note_ids)):
+                    fieldData = splitFields(flds)
+                    om = outputModels[mid]
+                    output.write('[[note]]\n')
+                    output.write("model = '%s'\n" % om.name)
+                    for i, name in enumerate(om.fieldNames):
+                        f = fieldData[i]
+                        if name == u'note-id':
+                            f = int(f)
+                        generator.gen_key_value(name, f)
+                    generator.gen_key_value(u'tags', tags.strip().decode('utf-8'))
+                    output.write('\n')
+                    count += 1
 
         self.count = count
+        return True
 
 
-def update_exporters_list(exps):
-    def _id(obj):
-        return "%s (*%s)" % (obj.key, obj.ext), obj
+# def update_exporters_list(exps):
+#     def _id(obj):
+#         return "%s (*%s)" % (obj.key, obj.ext), obj
+#
+#     exps.append(_id(TOMLNoteExporter))
+#
+#
+# addHook("exportersList", update_exporters_list)
 
-    exps.append(_id(TOMLNoteExporter))
-
-
-addHook("exportersList", update_exporters_list)
-
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
-
-class Ui_tomlExportDialog(object):
-    def setupUi(self, d):
-        d.setObjectName(_fromUtf8("tomlExportDlg"))
-        d.setWindowModality(QtCore.Qt.ApplicationModal)
-        d.resize(718, 358)
-        d.setSizeGripEnabled(True)
-        d.setModal(True)
-        self.verticalLayout = QtGui.QVBoxLayout(d)
-
-        grid = QtGui.QGridLayout()
-
-        self.fileEdit = QtGui.QLineEdit(d)
-        self.buttonBox = QtGui.QDialogButtonBox(d)
-        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
-        self.verticalLayout.addWidget(self.buttonBox)
-        #self.buttonBox.accepted = d.accept
-        #self.buttonBox.rejected = d.reject
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), d.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), d.reject)
-        QtCore.QMetaObject.connectSlotsByName(d)
-
-class ExportTOMLDialog(QDialog):
+class ExportDialog(QDialog):
 
     def __init__(self, mw):
         QDialog.__init__(self, mw, Qt.Window)
         self.mw = mw
-        self.ui = Ui_tomlExportDialog()
-        self.ui.setupUi(self)
-        self.setup()
+        self.setupUi()
+        self.fillValues()
 
-    def setup(self):
-        pass
+    def openProfile(self):
+        path_name = self.getProfilePathName()
+        if path_name:
+            self.profile_edit.setText(path_name)
 
+    def openOutput(self):
+        path_name = self.getOutputPathName()
+        if path_name:
+            self.output_edit.setText(path_name)
+
+    def onAccept(self):
+        ok = self.readValues()
+        if ok:
+            exporter = TOMLNoteExporter(mw.col, query=mw.ankisport.query, sets=mw.ankisport.sets)
+            ok = exporter.doExport(mw.ankisport.output_path)
+        if ok:
+            QDialog.accept(self)
+
+    def onReject(self):
+        self.close()
+
+    def getProfilePathName(self):
+        filter = 'TOML Files (*.toml)'
+        return unicode(QFileDialog.getOpenFileName(mw, "Exporter Profile",
+                                                   mw.ankisport.profile_path, filter))
+
+    def getOutputPathName(self):
+        filter = 'TOML Files (*.toml)'
+        return unicode(QFileDialog.getSaveFileName(mw, "Export to file",
+                                                   mw.ankisport.output_path, filter))
+
+    def fillValues(self):
+        self.profile_edit.setText(mw.ankisport.profile_path)
+        self.output_edit.setText(mw.ankisport.output_path)
+
+    def readValues(self):
+        mw.ankisport.profile_path = self.profile_edit.text()
+        if mw.ankisport.profile_path == "":
+            showWarning("The export profile is not set")
+            return False
+        mw.ankisport.output_path = self.output_edit.text()
+        if mw.ankisport.output_path == "":
+            showWarning("The export path is not set")
+            return False
+
+        with open(mw.ankisport.profile_path, 'r') as f:
+            t = toml.load(f)
+        mw.ankisport.query = t['query']
+        mw.ankisport.sets = t['sets']
+
+        return True
+
+    def setupUi(self):
+        self.setWindowModality(QtCore.Qt.ApplicationModal)
+        self.resize(718, 358)
+        self.setSizeGripEnabled(True)
+        self.setModal(True)
+
+        l_main = QtGui.QVBoxLayout(self)
+        grid = QtGui.QGridLayout()
+
+        profile_label = QLabel('Profile')
+        grid.addWidget(profile_label, 0, 0, 1, 1)
+        self.profile_edit = QLineEdit()
+        grid.addWidget(self.profile_edit, 0, 1, 1, 3)
+        profile_btn = QPushButton("Open &Profile", clicked=self.openProfile)
+        grid.addWidget(profile_btn, 0, 4, 1, 1)
+
+        output_label = QLabel('Output')
+        grid.addWidget(output_label, 1, 0, 1, 1)
+        self.output_edit = QLineEdit()
+        grid.addWidget(self.output_edit, 1, 1, 1, 3)
+        output_btn = QPushButton("Open &Output", clicked=self.openOutput)
+        grid.addWidget(output_btn, 1, 4, 1, 1)
+
+        l_main.addLayout(grid)
+        button_box = QDialogButtonBox(self)
+        button_box.setOrientation(QtCore.Qt.Horizontal)
+        button_box.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        button_box.accepted.connect(self.onAccept)
+        button_box.rejected.connect(self.onReject)
+        l_main.addWidget(button_box)
+        self.setLayout(l_main)
+
+class Settings(object):
+    def __init__(self):
+        dir = QDesktopServices.storageLocation(QDesktopServices.DesktopLocation)
+        self.profile_path = os.path.join(dir, "settings.toml")
+        self.output_path = os.path.join(dir, "export.toml")
+        self.query = ""
+        self.sets = []
 
 def displayDialog():
-    dlg = ExportTOMLDialog(mw)
+    dlg = ExportDialog(mw)
     dlg.exec_()
 
 action = QAction('TOML Export...', mw)
 action.triggered.connect(displayDialog)
 mw.form.menuTools.addAction(action)
-#mw.ExportToml = ExportSettings()
+mw.ankisport = Settings()
